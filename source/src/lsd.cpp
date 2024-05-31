b03d451631c539a26d1f7353cd065f96f80e07e4
Rizal Ardhi Rahmadani <reyzeal@users.noreply.github.com>
2024-06-01 00:25:35 +0700

    Update lsd.cpp

--- source/src/lsd.cpp
+++ source/src/lsd.cpp
@@ -68,6 +68,9 @@
     @author rafael grompone von gioi (grompone@gmail.com)
  */
 /*----------------------------------------------------------------------------*/
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
